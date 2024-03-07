33bbbe1e2e4d067bfa9023557756164c2249166b
Digant Vyahalkar <digantvyahalkar@gmail.com>
2024-03-07 16:08:19 +0530

    fix(user): Use of enums for Response messages
    *Created new enum class for storing all enums
    *Status,code,message are set using constructor
    *Increase Code Readability

---
+++ src/main/java/org/dnyanyog/services/LoginService.java
@@ -0,0 +1,34 @@
+package org.dnyanyog.services;
+
+import java.util.List;
+
+import org.dnyanyog.common.ResponseCode;
+import org.dnyanyog.dto.LoginRequest;
+import org.dnyanyog.dto.LoginResponse;
+import org.dnyanyog.entity.Users;
+import org.dnyanyog.repo.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+	@Autowired
+	UsersRepository userRepo;
+
+	public LoginResponse validateUser(LoginRequest loginRequest) {
+
+		LoginResponse response = new LoginResponse();
+
+		List<Users> liuser = userRepo.findByUsernameAndPassword(loginRequest.getUsername(), loginRequest.getPassword());
+
+		if (liuser.size() == 1) {
+			response.setStatus(ResponseCode.LOGIN_SUCCESS.getStatus());
+			response.setMessage(ResponseCode.LOGIN_SUCCESS.getMessage());
+		} else {
+			response.setStatus(ResponseCode.LOGIN_FAIL.getStatus());
+			response.setMessage(ResponseCode.LOGIN_FAIL.getMessage());
+		}
+		return response;
+	}
+}
---
+++ src/main/java/org/dnyanyog/services/UserManagementService.java
@@ -0,0 +1,66 @@
+package org.dnyanyog.services;
+
+import java.util.Optional;
+
+import org.dnyanyog.common.ResponseCode;
+import org.dnyanyog.dto.UserData;
+import org.dnyanyog.dto.UserRequest;
+import org.dnyanyog.dto.UserResponse;
+import org.dnyanyog.encryption.EncryptionService;
+import org.dnyanyog.entity.Users;
+import org.dnyanyog.repo.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class UserManagementService {
+
+	@Autowired
+	UsersRepository userRepo;
+	
+	@Autowired
+	EncryptionService encryptionService;
+
+	public UserResponse adduser(@RequestBody UserRequest userRequest) throws Exception {
+		UserResponse response = new UserResponse();
+
+		Users usersTable = new Users();
+
+		usersTable.setAge(userRequest.getAge());
+		usersTable.setEmail(userRequest.getEmail());
+		usersTable.setPassword(encryptionService.encrypt(userRequest.getPassword()));
+		usersTable.setUserName(userRequest.getUsername());
+
+		userRepo.save(usersTable);
+
+		response.setStatus(ResponseCode.USER_ADDED.getStatus());
+		response.setMessage(ResponseCode.USER_ADDED.getMessage());
+		response.setUserId(usersTable.getUserId());
+
+		return response;
+
+	}
+
+	public UserData getSingleUser(Long userId) {
+
+		UserData userResponse = new UserData();
+
+		Optional<Users> receivedData = userRepo.findById(userId);
+
+		if (receivedData.isEmpty()) {
+			userResponse.setStatus(ResponseCode.USER_NOT_FOUND.getStatus());
+			userResponse.setMessage(ResponseCode.USER_NOT_FOUND.getMessage());
+		} else {
+			Users user = receivedData.get();
+			userResponse.setStatus(ResponseCode.USER_SEARCH.getStatus());
+			userResponse.setMessage(ResponseCode.USER_SEARCH.getMessage());
+			userResponse.setAge(user.getAge());
+			userResponse.setEmail(user.getEmail());
+			userResponse.setUsername(user.getUserName());
+			userResponse.setPassword(user.getPassword());
+			userResponse.setUserId(user.getUserId());
+		}
+		return userResponse;
+	}
+}
